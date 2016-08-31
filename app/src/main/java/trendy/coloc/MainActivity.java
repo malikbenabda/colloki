c391db0882a172748c2aa467120841da374c0b32
malik.benabda <malik.benabda@gmail.com>
2016-08-31 09:05:13 +0100

    first Merge

--- app/src/main/java/trendy/coloc/MainActivity.java
+++ app/src/main/java/trendy/coloc/MainActivity.java
@@ -1,178 +1,166 @@
 package trendy.coloc;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
+
+
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import trendy.coloc.tools.UserPicture;
+import trendy.coloc.tools.NotificationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private NotificationManager myNotificationManager;
+    private int notificationIdOne = 111;
+    private int notificationIdTwo = 112;
+    private int numMessagesOne = 0;
+    private int numMessagesTwo = 0;
 
-    // this is the action code we use in our intent,
-    // this way we know we're looking at the response from our own action
-    private static final int SELECT_SINGLE_PICTURE = 101;
+    String ch ="{message:'+'success|failure'}";
 
-    private static final int SELECT_MULTIPLE_PICTURE = 201;
-
-    public static final String IMAGE_TYPE = "image/*";
-
-    private ImageView selectedImagePreview;
-    public static ArrayList<Bitmap> images;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // no need to cast to button view here since we can add a listener to any view, this
-        // is the single image selection
-        findViewById(R.id.btn_pick_single_image).setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View arg0) {
+        ScheduledExecutorService scheduler =
+                Executors.newSingleThreadScheduledExecutor();
 
-                // in onCreate or any event where your want the user to
-                // select a file
-                Intent intent = new Intent();
-                intent.setType(IMAGE_TYPE);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,
-                        getString(R.string.select_picture)), SELECT_SINGLE_PICTURE);
-            }
-        });
 
-        // multiple image selection
-        findViewById(R.id.btn_pick_multiple_images).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Intent intent = new Intent();
-                intent.setType(IMAGE_TYPE);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // this line is different here !!
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                startActivityForResult(Intent.createChooser(intent,
-                        getString(R.string.select_picture)), SELECT_MULTIPLE_PICTURE);
-            }
-        });
+        scheduler.scheduleAtFixedRate(new Runnable() {
 
-        selectedImagePreview = (ImageView) findViewById(R.id.image_preview);
-    }
+            public void run() {
+                Log.i("hello", "world");
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                      //  displayNotificationOne();
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_SINGLE_PICTURE) {
 
-                Uri selectedImageUri = data.getData();
-                try {
-                    selectedImagePreview.setImageBitmap(new UserPicture(selectedImageUri, getContentResolver()).getBitmap());
-                } catch (IOException e) {
-                    Log.e(MainActivity.class.getSimpleName(), "Failed to load image", e);
-                }
-                // original code
-//                String selectedImagePath = getPath(selectedImageUri);
-//                selectedImagePreview.setImageURI(selectedImageUri);
-            } else if (requestCode == SELECT_MULTIPLE_PICTURE) {
-                //And in the Result handling check for that parameter:
-                if (Intent.ACTION_SEND_MULTIPLE.equals(data.getAction())
-                        && data.hasExtra(Intent.EXTRA_STREAM)) {
-                    // retrieve a collection of selected images
-                    ArrayList<Parcelable> list = data.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-                    // iterate over these images
-                    if (list != null) {
-                        for (Parcelable parcel : list) {
-                            Uri uri = (Uri) parcel;
-                            try {
-                                images.add(new UserPicture(uri, getContentResolver()).getBitmap());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
+
+
+
                     }
+                });
 
-                    // for now just show the last picture
-                    if (!list.isEmpty()) {
-                        Uri imageUri = (Uri) list.get(list.size() - 1);
-
-                        try {
-                            selectedImagePreview.setImageBitmap(new UserPicture(imageUri, getContentResolver()).getBitmap());
-                        } catch (IOException e) {
-                            Log.e(MainActivity.class.getSimpleName(), "Failed to load image", e);
-                        }
-                        // original code
-//                        String selectedImagePath = getPath(imageUri);
-//                        selectedImagePreview.setImageURI(imageUri);
-//                        displayPicture(selectedImagePath, selectedImagePreview);
-                    }
-                }
             }
-        } else {
-            // report failure
-            Toast.makeText(getApplicationContext(), R.string.msg_failed_to_get_intent_data, Toast.LENGTH_LONG).show();
-            Log.d(MainActivity.class.getSimpleName(), "Failed to get intent data, result code is " + resultCode);
-        }
+        }, 10, 10, TimeUnit.SECONDS);
+
+
     }
 
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
+    protected void displayNotificationOne() {
 
-        // just some safety built in
-        if (uri == null) {
-            // perform some logging or show user feedback
-            Toast.makeText(getApplicationContext(), R.string.msg_failed_to_get_picture, Toast.LENGTH_LONG).show();
-            Log.d(MainActivity.class.getSimpleName(), "Failed to parse image path from image URI " + uri);
-            return null;
-        }
+        // Invoking the default notification service
+        NotificationCompat.Builder  mBuilder = new NotificationCompat.Builder(this);
 
-        // try to retrieve the image from the media store first
-        // this will only work for images selected from gallery
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if (cursor != null) {
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        }
-        // this is our fallback here, thanks to the answer from @mad indicating this is needed for
-        // working code based on images selected using other file managers
-        return uri.getPath();
+        mBuilder.setContentTitle("New Message with explicit intent");
+        mBuilder.setContentText("New message from javacodegeeks received");
+        mBuilder.setTicker("Explicit: New Message Received!");
+        mBuilder.setSmallIcon(R.drawable.redp);
+
+        // Increase notification number every time a new notification arrives
+        mBuilder.setNumber(++numMessagesOne);
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, NotificationView.class);
+        resultIntent.putExtra("notificationId", notificationIdOne);
+
+        //This ensures that navigating backward from the Activity leads out of the app to Home page
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Adds the back stack for the Intent
+        stackBuilder.addParentStack(NotificationView.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_ONE_SHOT //can only be used once
+                );
+        // start the activity when the user clicks the notification text
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        myNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // pass the Notification object to the system
+        myNotificationManager.notify(notificationIdOne, mBuilder.build());
     }
 
 
-//    /**
-//     * helper to scale down image before display to prevent render errors:
-//     * "Bitmap too large to be uploaded into a texture"
-//     */
-//    private void displayPicture(String imagePath, ImageView imageView) {
-//
-//        // from http://stackoverflow.com/questions/22633638/prevent-bitmap-too-large-to-be-uploaded-into-a-texture-android
-//
-//        BitmapFactory.Options options = new BitmapFactory.Options();
-//        options.inSampleSize = 4;
-//
-//        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-//        int height = bitmap.getHeight(), width = bitmap.getWidth();
-//
-//        if (height > 1280 && width > 960){
-//            Bitmap imgbitmap = BitmapFactory.decodeFile(imagePath, options);
-//            imageView.setImageBitmap(imgbitmap);
-//        } else {
-//            imageView.setImageBitmap(bitmap);
-//        }
-//    }
+    public void  ConnNative(View v){
+
+
+        Intent i = new Intent(MainActivity.this,ConnectionNative.class);
+        startActivity(i);
+
+
+    }
+
+
+    public void  ConnFb(View v){
+
+
+        Intent i = new Intent(MainActivity.this,FbLogin.class);
+        startActivity(i);
+
+
+    }
+
+    public void  Register(View v){
+
+
+        Intent i = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(i);
+
+
+    }
+
+
+    public void  DmdColoc(View v){
+
+
+        Intent i = new Intent(MainActivity.this,DemandeColocationActivity.class);
+        startActivity(i);
+
+
+    }
+
+
+    public void  ChercherDmdColoc(View v){
+
+
+        Intent i = new Intent(MainActivity.this,DemandeColocationSearchActivity.class);
+        startActivity(i);
+
+
+    }
+
+    public void  ListeDemandes(View v){
+
+
+        Intent i = new Intent(MainActivity.this,ListeDesDemandesActivity.class);
+        startActivity(i);
+
+
+    }
 
 
 }
+
+
+
--- app/src/main/java/trendy/coloc/tools/SaveSharedPreference.java
+++ app/src/main/java/trendy/coloc/tools/SaveSharedPreference.java
@@ -12,16 +12,18 @@ import android.preference.PreferenceManager;
 /**
  * Created by thegohst on 7/23/16.
  */
-public class SaveSharedPreference {
-    static final String PREF_USER_NAME = "";
-    static final String PREF_USER_PHOTO = "";
-    static final String PREF_USER_ID = "";
+public class SaveSharedPreference
+{
+    static final String PREF_USER_NAME= "";
+    static final String PREF_USER_PHOTO= "";
+    static final String PREF_USER_ID="";
 
     static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
     }
 
-    public static void setUserName(Context ctx, String userName) {
+    public static void setUserName(Context ctx, String userName)
+    {
 
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putString(PREF_USER_NAME, userName);
@@ -29,28 +31,33 @@ public class SaveSharedPreference {
     }
 
 
-    public static void setUserId(Context ctx, String id) {
+    public static void setUserId(Context ctx, String id)
+    {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putString(PREF_USER_NAME, id);
         editor.commit();
     }
 
 
-    public static void setUserPhoto(Context ctx, String userPhoto) {
+    public static void setUserPhoto(Context ctx, String userPhoto)
+    {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putString(PREF_USER_PHOTO, userPhoto);
         editor.commit();
     }
 
-    public static String getUserName(Context ctx) {
+    public static String getUserName(Context ctx)
+    {
         return getSharedPreferences(ctx).getString(PREF_USER_NAME, "");
     }
 
-    public static String getUserId(Context ctx) {
+    public static String getUserId(Context ctx)
+    {
         return getSharedPreferences(ctx).getString(PREF_USER_ID, "");
     }
 
-    public static String getUserPhoto(Context ctx) {
+    public static String getUserPhoto(Context ctx)
+    {
         return getSharedPreferences(ctx).getString(PREF_USER_PHOTO, "");
     }
 }
